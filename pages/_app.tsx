4c2d4b904afc8e5209b3799a723da8e477de3603
Brandon Fan <brandonfan1256@gmail.com>
2021-11-24 14:41:55 -0600

    fix issue with analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,16 @@ import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import { app } from "@/libs/firebase";
 import { PlayerProvider } from "@/contexts/player";
+import { useEffect } from "react";
+import { getAnalytics } from "@firebase/analytics";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      getAnalytics(app);
+    }
+  }, [])
   return (
     <PlayerProvider>
       <Component {...pageProps} />
